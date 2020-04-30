8c14ba35111baad495d5aaa523f22ee79038a168
Роман Комаров <rom541997@gmail.com>
2020-04-30 22:59:58 +0700

    FIX: Deploy config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/semen-platformer-prod/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/pfdo-platformer-prod/' : '/',
 	css: {
 		loaderOptions: {
 			sass: {
