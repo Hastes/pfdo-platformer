f4b466517b4e68760b918cd10b3feaf06641930b
bikishovd <bikishovd.com>
2020-04-30 14:24:43 +0300

    add rotation

--- src/components/PfdoGameLogo.vue
+++ src/components/PfdoGameLogo.vue
@@ -2,9 +2,37 @@
 h1.pfdo-game-logo PFDO Gaming
 </template>
 
-
 <style scoped lang="scss">
 .pfdo-game-logo {
-  color: $primary-color;
+	display: block;
+	text-align: center;
+	color: $primary-color;
+	transition: all 0.5s ease;
+	animation: 0.4s linear 0s infinite alternate anim;
+	transform: rotate(0);
+
+	&:hover {
+		display: block;
+		color: $dark-color;
+		transform: rotate(45deg);
+		animation: 0.4s linear 0s infinite rotation;
+	}
+}
+
+@keyframes anim {
+	from {
+		transform: scale(1.2);
+	}
+	to {
+		transform: scale(0.9);
+	}
+}
+@keyframes rotation {
+	from {
+		transform: rotate(0);
+	}
+	to {
+		transform: rotate(360deg);
+	}
 }
 </style>
